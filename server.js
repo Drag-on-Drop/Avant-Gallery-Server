0692fabd21fcd6e165792db1678c2a46fff2f680
stevendo <stevendo@me.com>
2020-07-15 09:39:29 -0400

    fixed serverjs file

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 
 // require route files
-const exampleRoutes = require('./app/routes/example_routes')
+// const exampleRoutes = require('./app/routes/example_routes')
 const userRoutes = require('./app/routes/user_routes')
+const artworkRoutes = require('./app/routes/artwork_routes')
+const collectionRoutes = require('./app/routes/collection_routes')
 
 // require middleware
 const errorHandler = require('./lib/error_handler')
@@ -61,8 +63,10 @@ app.use(express.urlencoded({ extended: true }))
 app.use(requestLogger)
 
 // register route files
-app.use(exampleRoutes)
+// app.use(exampleRoutes)
 app.use(userRoutes)
+app.use(artworkRoutes)
+app.use(collectionRoutes)
 
 // register error handling middleware
 // note that this comes after the route middlewares, because it needs to be
