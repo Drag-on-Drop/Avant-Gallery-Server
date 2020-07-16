ab14c65f55b114ed5b49e9176113f5dd08ab428f
stevendo <stevendo@me.com>
2020-07-16 16:52:29 -0400

    added show artist route and curl scripts

---
+++ curl-scripts/auth/show-artist.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+API="http://localhost:4741"
+URL_PATH="/artists"
+
+curl "${API}${URL_PATH}/${ID}" \
+  --include \
+  --request GET \
+  # --header "Authorization: Bearer ${TOKEN}"
+
+echo
