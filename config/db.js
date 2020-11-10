357f83b634f91d55110aeef6f693e035205aa3a5
jasonfiammetta <jason.fiammetta@gmail.com>
2020-11-09 20:44:48 -0500

    Changed database string

--- config/db.js
+++ config/db.js
@@ -15,6 +15,6 @@ const localDb = process.env.TESTENV ? database.test : database.development
 
 // Environment variable MONGODB_URI will be available in
 // heroku production evironment otherwise use test or development db
-const currentDb = process.env.MONGODB_URI || localDb
+const currentDb = process.env.DB_URI || localDb
 
 module.exports = currentDb
